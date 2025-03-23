2a4e9c4a1824f0357c627ca2410530ed064ad0c7
Sush3871 <sushanrshirke96.com>
2025-03-23 23:30:08 +0530

    last changes

--- orchid_hotels/custom_admin/views.py
+++ orchid_hotels/custom_admin/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.contrib.auth.forms import PasswordResetForm
-from .forms import CustomPasswordResetForm
+from .forms import CustomPasswordResetForm 
 
 
 def admin_login(request):
