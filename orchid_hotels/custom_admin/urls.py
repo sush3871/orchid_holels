7fe34fd73a53f9dde56297fc09a8d0ffc99b7d5e
Sush3871 <sushanrshirke96.com>
2025-03-29 15:25:32 +0530

    New changes

--- orchid_hotels/custom_admin/urls.py
+++ orchid_hotels/custom_admin/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from .views import admin_login, admin_dashboard, admin_logout, admin_signup, password_reset, password_reset_confirm
 
 urlpatterns = [
-    path('', admin_login, name='admin_login'),  # Default redirects to login
+
+    # Login pages 
+    path('', admin_login, name='admin_login'),  
     path('sign-in/', admin_login, name='admin_login'),
-    path('dashboard/', admin_dashboard, name='admin_dashboard'),
     path('logout/', admin_logout, name='admin_logout'),
     path('sign-up/', admin_signup, name='admin_signup'),
     path('forgot-password/', password_reset, name='password_reset'),
     path('password_reset_confirm/<uidb64>/<token>/', password_reset_confirm, name='password_reset_confirm'),
+
+    # Dashboard Pages 
+    path('dashboard/', admin_dashboard, name='admin_dashboard'),
 
 ]
